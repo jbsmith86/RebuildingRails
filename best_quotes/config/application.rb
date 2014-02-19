a80d4634aed07158814f9c242f008688aa950cca
Joel Smith <joely5525@gmail.com>
2014-02-18 17:18:53 -0800

    chapter 2 code

--- best_quotes/config/application.rb
+++ best_quotes/config/application.rb
@@ -1,4 +1,9 @@
 require "rulers"
+require "./app/controllers/quotes_controller"
+
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "controllers")
 
 module BestQuotes
   class Application < Rulers::Application
